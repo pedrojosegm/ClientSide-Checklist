cbf502947c3111a8a83bb3bc57d8425930b8e3ed
pedrojosegm <pedrojosegm@gmail.com>
2012-12-17 10:53:17 -0600

    add second round of checklist

---
+++ checklist/js/moreInfo.js
@@ -0,0 +1,15 @@
+$('p.infoPos').mouseover(function() {
+	$('p.moreInfo').addClass('infoExpand');
+});
+   		
+$('p.infoPos').mouseleave(function() {
+	$('p.moreInfo').removeClass('infoExpand');
+});
+
+$('p.moreInfo').mouseover(function() {
+	$('p.moreInfo').addClass('infoExpand');
+});
+   		
+$('p.moreInfo').mouseleave(function() {
+	$('p.moreInfo').removeClass('infoExpand');
+});
